9cfad87ff2bdcda6051c10aad4ebdf1e352f14e1
Daniel Sposito <daniel.g.sposito@gmail.com>
2015-10-23 01:58:01 -0700

    Adds the ability to provide meta data when getting an instance of a provider.

--- src/Provider.php
+++ src/Provider.php
@@ -2,22 +2,56 @@
 
 namespace Extractor;
 
-class Provider
+/**
+ * Adapter class that handles extractor provider interactions.
+ */
+abstract class Provider
 {
     /**
      * Gets an instance of the given provider.
      *
      * @param string $provider The name of the provider to instantiate.
+     * @param array $data Optional provider data.
      *
      * @return Provider|bool
      */
-    public static function instance($provider)
+    public static function instance($provider, $data = array())
     {
         $class = __NAMESPACE__ . '\\Provider\\' . ucfirst($provider);
         if (!class_exists($class)) {
             return false;
         }
 
-        return new $class();
+        return new $class($data);
+    }
+
+    /**
+     * Initializes the class.
+     *
+     * @param array $data
+     * 
+     * @return void
+     */
+    public function __construct(array $data = array())
+    {
+        if (!empty($data)) {
+            $this->loadData($data);
+        }
+    }
+
+    /**
+     * Attempts to map array data to object properties.
+     *
+     * @param array $data
+     *
+     * @return void
+     */
+    public function loadData(array $data = array())
+    {
+        foreach ($data as $key => $value) {
+            if (property_exists($this, $key)) {
+                $this->$key = $value;
+            }
+        }
     }
 }

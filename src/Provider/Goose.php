dd26b2a9aeab319966a23de1ca3270439ebcdf3d
Daniel Sposito <daniel.g.sposito@gmail.com>
2015-10-23 02:41:44 -0700

    Implements a method for extracting content via the Goose provider.

--- src/Provider/Goose.php
+++ src/Provider/Goose.php
@@ -2,9 +2,30 @@
 
 namespace Extractor\Provider;
 
+use Extractor\Content;
 use Extractor\Provider;
+use Goose\Client as GooseSDK;
 
+/**
+ * Driver class that handles Goose interactions.
+ */
 class Goose extends Provider
 {
-    
+    /**
+     * Extracts content for a given URL.
+     *
+     * @param string $url The URL to extract content from.
+     *
+     * @return Content
+     */
+    public function extract($url)
+    {
+        $extractor = new GooseSDK(get_object_vars($this));
+        $data = $extractor->extractContent($url);
+
+        $content = new Content();
+        $content->text = $data->getCleanedArticleText();
+
+        return $content;
+    }
 }

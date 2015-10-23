5ae1befce8b2cf03bcce818a9eb224b089a6cd51
Daniel Sposito <daniel.g.sposito@gmail.com>
2015-10-23 01:58:31 -0700

    Adds a class for handling extracted content.

---
+++ src/Content.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Extractor;
+
+/**
+ * Handles various extracted content types.
+ */
+class Content
+{
+    /**
+     * Extracted text content.
+     *
+     * @var string
+     */
+    public $text;
+}

b7b2f2906568db40288ac4c5a31ac5cb668c26d5
Daniel Sposito <daniel.g.sposito@gmail.com>
2017-01-20 03:18:31 -0800

    Adds unit tests.

---
+++ tests/Unit/ProviderTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace DayRev\Extractor\Tests\Unit;
+
+use DayRev\Extractor\Provider;
+
+class ProviderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testProviderIsGoose()
+    {
+        $provider = Provider::instance('goose');
+
+        $this->assertInstanceOf('DayRev\Extractor\Provider\Goose', $provider);
+    }
+
+    public function testProviderIsEmbedly()
+    {
+        $provider = Provider::instance('embedly');
+
+        $this->assertInstanceOf('DayRev\Extractor\Provider\Embedly', $provider);
+    }
+
+    public function testProviderIsInvalid()
+    {
+        $provider = Provider::instance('caterpillar');
+
+        $this->assertFalse($provider);
+    }
+
+    public function testProviderMetaDataIsSet()
+    {
+        $provider = Provider::instance('embedly', array('api_key' => 'SDGJ8924TFDSF713J'));
+
+        $this->assertObjectHasAttribute('api_key', $provider);
+        $this->assertAttributeEquals('SDGJ8924TFDSF713J', 'api_key', $provider);
+    }
+}
